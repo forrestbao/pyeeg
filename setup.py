191a406b07f9f4697f3eb0a7e5f72f54bdf31dd6
Joe Bergantine <jbergantine@gmail.com>
2015-07-10 15:44:27 -0600

    changed name in setup.py; fixed carryover author email attribute and value

--- setup.py
+++ setup.py
@@ -1,12 +1,11 @@
 from setuptools import setup
 
 setup(
-    name='pyEEG',
+    name='pyeeg',
     version='v0.03',
     description='A Python module to extract EEG features.',
     url='https://github.com/forrestbao/pyeeg',
     author='Forrest Bao',
-    author_email='admin@4csports.com',
     license='GNU v3',
     packages=['pyeeg'],
     install_requires=[
