18e2fcbd9814610d6527ce16a90efc10fce5c7de
Forrest Bao <forrest.bao@gmail.com>
2015-12-19 02:52:39 -0500

    update readme to reflect the contributions from many people.

--- setup.py
+++ setup.py
@@ -4,10 +4,10 @@ from setuptools import setup
 setup(
     name='pyeeg',
     version='0.4.0',
-    description='A Python module to extract EEG features.',
+    description='A Python module to extract EEG features',
     url='https://github.com/forrestbao/pyeeg',
-    author='Forrest Bao',
-    license='GNU',
+    author='Forrest Bao and all contributors',
+    license='GNU GPL v3',
     packages=['pyeeg'],
     install_requires=[
         'numpy>=1.9.2',
