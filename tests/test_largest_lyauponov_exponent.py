b6cf54f3dbe52f9154bd8c166c6a66339920cd1b
Joe Bergantine <jbergantine@gmail.com>
2016-12-13 16:59:43 -0700

    adjusted unit test, travis differs after the 14th decimal point or so

--- tests/test_largest_lyauponov_exponent.py
+++ tests/test_largest_lyauponov_exponent.py
@@ -8,9 +8,10 @@ class LLETests(unittest.TestCase):
     def test_largest_lyauponov_exponent(self):
         data = numpy.asarray([3, 4, 1, 2, 4, 51, 4, 32, 24, 12, 3, 45])
 
-        self.assertEqual(
+        self.assertAlmostEqual(
             LLE(data, 2, 4, 1, 1),
-            0.18771136179353307
+            0.18771136179353307,
+            places=12
         )
 
 
