fc70d744bf00b74057b23a87532df289c1595848
Joe Bergantine <jbergantine@gmail.com>
2016-12-13 16:55:39 -0700

    enforce sequence as array type

--- pyeeg/embedded_sequence.py
+++ pyeeg/embedded_sequence.py
@@ -1,27 +1,31 @@
 import numpy
 
 
-def embed_seq(X, Tau, D):
-    """Build a set of embedding sequences from given time series X with lag Tau
-    and embedding dimension DE. Let X = [x(1), x(2), ... , x(N)], then for each
-    i such that 1 < i <  N - (D - 1) * Tau, we build an embedding sequence,
-    Y(i) = [x(i), x(i + Tau), ... , x(i + (D - 1) * Tau)]. All embedding
-    sequence are placed in a matrix Y.
+def embed_seq(time_series, tau, embedding_dimension):
+    """Build a set of embedding sequences from given time series `time_series`
+    with lag `tau` and embedding dimension `embedding_dimension`.
+
+    Let time_series = [x(1), x(2), ... , x(N)], then for each i such that
+    1 < i <  N - (embedding_dimension - 1) * tau,
+    we build an embedding sequence,
+    Y(i) = [x(i), x(i + tau), ... , x(i + (embedding_dimension - 1) * tau)].
+
+    All embedding sequences are placed in a matrix Y.
 
     Parameters
     ----------
 
-    X
-        list
+    time_series
+        list or numpy.ndarray
 
         a time series
 
-    Tau
+    tau
         integer
 
         the lag or delay when building embedding sequence
 
-    D
+    embedding_dimension
         integer
 
         the embedding dimension
@@ -30,7 +34,7 @@ def embed_seq(X, Tau, D):
     -------
 
     Y
-        2-D list
+        2-embedding_dimension list
 
         embedding matrix built
 
@@ -63,6 +67,20 @@ def embed_seq(X, Tau, D):
            [ 8.]])
 
     """
-    shape = (X.size - Tau * (D - 1), D)
-    strides = (X.itemsize, Tau * X.itemsize)
-    return numpy.lib.stride_tricks.as_strided(X, shape=shape, strides=strides)
+    if not type(time_series) == numpy.ndarray:
+        typed_time_series = numpy.asarray(time_series)
+    else:
+        typed_time_series = time_series
+
+    shape = (
+        typed_time_series.size - tau * (embedding_dimension - 1),
+        embedding_dimension
+    )
+
+    strides = (typed_time_series.itemsize, tau * typed_time_series.itemsize)
+
+    return numpy.lib.stride_tricks.as_strided(
+        typed_time_series,
+        shape=shape,
+        strides=strides
+    )
